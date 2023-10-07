d14fef0ec371cf7a58672e8eb1aba6f4f7fcdce4
kobinarth22@gmail.com <kobinarth22@gmail.com>
2023-10-07 16:46:56 +0530

    singleton pattern

---
+++ Singleton/src/Singleton.java
@@ -0,0 +1,12 @@
+public class Singleton {
+    private static Singleton obj;
+    private Singleton(){
+    };
+    public static synchronized Singleton getInstance()
+    {
+        if (obj == null) {
+            obj = new Singleton();
+        };
+        return obj;
+    };
+}
